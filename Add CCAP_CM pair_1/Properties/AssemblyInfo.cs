430ae727f1513b13bb9f7f0a4ecaa93baaa369fd
Maria Cunha <maria.cunha@skyline.be>
2023-04-04 11:00:59 +0100

    Add project files.

---
+++ Add CCAP_CM pair_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Add CCAP_CM pair_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Add CCAP_CM pair_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A504BBBD-152D-4383-9BDF-DBB03AD1D490")]
+[assembly: AssemblyVersion("1.0.0.0")]
